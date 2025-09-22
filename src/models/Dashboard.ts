645552474936c5086728b54677204218f8a656a3
Thales Manoel <thalesdesenvolvedor@gmail.com>
2025-09-21 21:28:17 -0300

    feat: create dashboard model

---
+++ src/models/Dashboard.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose'
+
+export interface IDashboard extends Document {
+    billingTotalValue?: number;
+    servicesTotalValue?: number;
+    productsTotalValue?: number;
+    costsTotalValue?: number;
+    grossProfitTotalValue?: number;
+    quantityServiceOrdersCompleted?: number;
+    quantityNewClients?: number;
+}
+
+const DashboardSchema = new Schema<IDashboard>(
+  {
+    billingTotalValue: { type: Number },
+    servicesTotalValue: { type: Number },
+    productsTotalValue: { type: Number },
+    costsTotalValue: { type: Number },
+    grossProfitTotalValue: { type: Number },
+    quantityServiceOrdersCompleted: { type: Number },
+    quantityNewClients: { type: Number }
+  },
+  { timestamps: true }
+)
+
+export default mongoose.model<IDashboard>('Dashboard', DashboardSchema)
