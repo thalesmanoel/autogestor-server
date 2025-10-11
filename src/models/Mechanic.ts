f26e8c25fbbeda9865fe052377709359b30b46ce
Thales Manoel <thalesdesenvolvedor@gmail.com>
2025-10-11 15:08:58 -0300

    feat: add new fields in Mechanic Model

--- src/models/Mechanic.ts
+++ src/models/Mechanic.ts
@@ -3,16 +3,30 @@ import mongoose, { Document, Schema } from 'mongoose'
 export interface IMechanic extends Document {
     name: string;
     email: string;
+    cpf: string;
     cellphone: string;
+    position: string;
+    cep: string;
     address: string;
+    number: string;
+    city: string;
+    state: string;
+    notes?: string;
 }
 
 const MechanicSchema = new Schema<IMechanic>(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    cpf: { type: String, required: true, unique: true },
     cellphone: { type: String, required: true },
-    address: { type: String, required: true }
+    position: { type: String, required: true },
+    cep: { type: String, required: true },
+    address: { type: String, required: true },
+    number: { type: String, required: true },
+    city: { type: String, required: true },
+    state: { type: String, required: true },
+    anotations: { type: String }
   },
   { timestamps: true }
 )
