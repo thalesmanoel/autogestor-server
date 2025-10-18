d82a93c7ac6a3184485a2a21387561ef86f352c6
Thales Manoel <thalesdesenvolvedor@gmail.com>
2025-10-17 23:06:52 -0300

    feat: block delete of client if he is associated to service order

--- src/controllers/auth/ClientController.ts
+++ src/controllers/auth/ClientController.ts
@@ -31,7 +31,7 @@ export default class ClientController {
   getById = async (req: Request, res: Response, next: NextFunction) => {
     try {
       const client = await this.clientService.findById(new Types.ObjectId(req.params.id))
-      if (!client) return res.status(404).json({ message: 'Produto não encontrado' })
+      if (!client) return res.status(404).json({ message: 'Cliente não encontrado' })
       res.json(client)
     } catch (error) {
       next(error)
@@ -44,7 +44,7 @@ export default class ClientController {
       const data = req.body
 
       const client = await this.clientService.update(new Types.ObjectId(id), data)
-      if (!client) return res.status(404).json({ message: 'Produto não encontrado' })
+      if (!client) return res.status(404).json({ message: 'Cliente não encontrado' })
       res.json(client)
     } catch (error) {
       next(error)
@@ -53,9 +53,8 @@ export default class ClientController {
 
   delete = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const client = await this.clientService.delete(new Types.ObjectId(req.params.id))
-      if (!client) return res.status(404).json({ message: 'Produto não encontrado' })
-      res.json({ message: 'Produto deletado com sucesso' })
+      await this.clientService.deleteClient(new Types.ObjectId(req.params.id))
+      res.json({ message: 'Cliente deletado com sucesso' })
     } catch (error) {
       next(error)
     }
--- src/services/ClientService.ts
+++ src/services/ClientService.ts
@@ -1,9 +1,22 @@
+import { Types } from 'mongoose'
+
 import { IClient } from '../models/Client'
+import ServiceOrder from '../models/ServiceOrder'
 import ClientRepository from '../repositories/ClientRepository'
 import BaseService from './BaseService'
 
 export default class ClientService extends BaseService<IClient> {
   constructor () {
     super(new ClientRepository())
+  }
+
+  async deleteClient (id: Types.ObjectId): Promise<void> {
+    if (!id) throw new Error('ID do cliente é obrigatório para deletar.')
+
+    const order = await ServiceOrder.findOne({ clientId: id }).select('_id').lean()
+
+    if (order) throw new Error('Não é possível deletar um cliente com ordens de serviço associadas.')
+
+    await this.repository.delete(id)
   }
 }
